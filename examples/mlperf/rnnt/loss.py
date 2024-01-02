08b58cdfafa04e2ccf41d1943c0e52a5063fc858
Francis Lata <francisalbertlata@gmail.com>
2024-01-02 12:04:20 +0000

    add transducer loss

---
+++ examples/mlperf/rnnt/loss.py
@@ -0,0 +1,15 @@
+import torch
+import torchaudio.functional as F
+
+from tinygrad import Tensor
+
+
+class TransducerLoss:
+  def __init__(self, blank_idx:int):
+    self.blank_idx = blank_idx
+
+  def __call__(self, y_:Tensor, y_lens_:Tensor, y:Tensor, y_lens:Tensor):
+    y_, y_lens_ = torch.Tensor(y_.numpy(), dtype=torch.float), torch.Tensor(y_lens_.numpy(), dtype=torch.int)
+    y, y_lens = torch.Tensor(y.numpy(), dtype=torch.int), torch.Tensor(y_lens.numpy(), dtype=torch.int)
+    loss = F.rnnt_loss(y_, y, y_lens_, y_lens, blank=self.blank_idx)
+    return Tensor(loss.numpy(), requires_grad=True)
