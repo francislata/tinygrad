949a1c2444a3a9bf0be9b60c6d2693a1bcea3337
Francis Lata <francisalbertlata@gmail.com>
2024-01-28 18:05:51 +0000

    add focal loss

---
+++ examples/mlperf/retinanet/losses.py
@@ -0,0 +1,16 @@
+from tinygrad import Tensor
+
+def sigmoid_focal_loss(inputs:Tensor, targets:Tensor, alpha:float = 0.25, gamma:float = 2, reduction:str = "none") -> Tensor:
+  p = inputs.sigmoid()
+  # NOTE: from binary_crossentropy_logits implementation without mean reduction
+  ce_loss = inputs.maximum(0) - targets * inputs + (1 + inputs.abs().neg().exp()).log()
+  p_t = p * targets + (1 - p) * (1 - targets)
+  loss = ce_loss * ((1 - p_t) ** gamma)
+
+  if alpha >= 0:
+    alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+    loss *= alpha_t
+
+  if reduction == "mean": loss = loss.mean()
+  elif reduction == "sum": loss = loss.sum()
+  return loss
